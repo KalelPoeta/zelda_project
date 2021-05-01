2e665f2ab50e4cb5b991afcdce00b10cf95f8c62
Kalel <kalel@lelak.ch>
2021-05-01 21:10:17 +0200

    Ammo-collecting-system & UI ammo count done

--- src/com/LelakGames/entities/Player.java
+++ src/com/LelakGames/entities/Player.java
@@ -19,6 +19,8 @@ public class Player  extends Entity{
 	private BufferedImage[] rightPlayer;
 	private BufferedImage[] leftPlayer;
 	
+	public int ammo = 0;
+	
 	public static double life = 100, maxlife = 100;
 
 	public Player(int x, int y, int width, int height, BufferedImage sprite) {
@@ -68,12 +70,29 @@ public class Player  extends Entity{
 			}
 		}
 		
-		this.checkCollisionLifePack();
+		checkCollisionLifePack();
+		checkCollisionAmmo();							
 		
 		Camera.x = Camera.clamp (this.getX() - (Game.WIDTH/2), 0, World.WIDTH*16 - Game .WIDTH) ;
 		Camera.y = Camera.clamp (this.getY() - (Game.HEIGHT/2), 0, World.HEIGHT*16 - Game.HEIGHT) ;
 		
      }
+        
+	
+	public void checkCollisionAmmo(){
+		for(int i = 0; i < Game.entities.size(); i++) {
+			Entity atual = Game.entities.get(i);
+			if(atual instanceof Ammo) {
+				if(Entity.isColidding(this, atual)) {
+					ammo++;
+					System.out.println("Actual ammo:" + ammo); 
+					Game.entities.remove(atual);
+				}
+			}
+			
+		}
+		
+	}
 	
 	public void checkCollisionLifePack() {
 		for(int i = 0; i < Game.entities.size(); i++) {
@@ -89,6 +108,8 @@ public class Player  extends Entity{
 			
 		}
 	}
+	
+	
 	
 	
 	public void render(Graphics g) {
--- src/com/LelakGames/main/Game.java
+++ src/com/LelakGames/main/Game.java
@@ -4,6 +4,7 @@ import java.awt.Canvas;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -130,6 +131,9 @@ public class Game extends Canvas implements Runnable,KeyListener{
 		g.dispose();
 		g = bs.getDrawGraphics();
 		g.drawImage(image, 0, 0,WIDTH*SCALE,HEIGHT*SCALE,null);
+		g.setFont(new Font ("arial", Font.BOLD, 25));
+		g.setColor(Color.magenta);
+		g.drawString("Ammo : " + player.ammo, 600, 20);
 		bs.show();
 	}
 
